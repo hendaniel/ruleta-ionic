07992d5b0f2d715dae3e0fb8df29c7ac3a53f25e
CodeDanielMartinez <danielmartinezdm@hotmail.com>
2019-09-05 12:13:11 -0500

    Logica ya implementada

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+
 })
+
 export class HomePage {
 
-  constructor() {}
+  text: string = "";
+  elements: string[];
+  final: string = "";
+  constructor(public navCtrl: NavController) {
 
+  }
+  sort() {
+    if (this.text != null && this.text.trim() != '') {
+      this.elements = this.text.split(',');
+      for (var i = 0; i < 10; i++) {
+        let k = i;
+        setTimeout(()=> {
+          this.final = this.elements[Math.floor(Math.random() * (this.elements.length))];
+        }, 100 * (k + 1));
+      }
+    }
+    else {
+      alert("Ingresa Texto");
+    }
+    console.log(this.elements);
+  }
 }
